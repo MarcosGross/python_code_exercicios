de5024585da8a06b55495eba823d3579f679b5e7
Marcos <marcosgrossmoraes@gmail.com>
2019-08-26 19:34:38 -0300

    prepara arquivos ruby para exercícios sequencias

---
+++ 01_sequenciais/exercicio_sequencial_1.rb
@@ -0,0 +1,9 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+# Formula matemática:
+#      ℉ - 32
+# ℃ = ________  OU  F= 1,8C + 32
+#        1,8
+
+puts "Digite a temperatura: "
+graus_celsius = gets.to_f
+
